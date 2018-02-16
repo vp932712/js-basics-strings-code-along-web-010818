84134647479519f4b05aba2472ea70a13154a10c
Vaibhav Patel <vp932712@gmail.com>
2018-02-16 15:33:17 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+var currentUser = "vic"
+
+var welcomeMessage = "Welcome to Flatbook, " + currentUser+"!"
+
+var excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase()+"!"
+
+var shortGreeting = "Welcome, " + currentUser.charAt(0)+"!"
